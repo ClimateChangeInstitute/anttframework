a4e57425d89286f95075ffb9897866f932b94cfd
markroyer <mark.e.royer@gmail.com>
2016-09-24 11:33:35 -0400

    Add example servlet and unit test

---
+++ AntT/src/org/tephrochronology/ExampleTest.java
@@ -0,0 +1,35 @@
+package org.tephrochronology;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This example test should be removed once actual tests are created.
+ * 
+ * @author Mark Royer
+ *
+ */
+public class ExampleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		assertTrue("Just an example that should be removed", true);
+	}
+
+	@Test
+	public void testDoPostHttpServletRequestHttpServletResponse() {
+		assertTrue("Just an example that should be removed", true);
+	}
+
+}
