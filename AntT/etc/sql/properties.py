6abc96cf97a35f4f4fb3a2bf640f2426ef669551
markroyer <mark.e.royer@gmail.com>
2016-10-25 10:54:46 -0400

    Fix antt db and backup file names

--- AntT/etc/sql/properties.py
+++ AntT/etc/sql/properties.py
@@ -2,10 +2,10 @@
 
 from admin import THE_ADMIN, THE_ADIM_PASS
 
-DATABASE = "ant"
+DATABASE = "antt"
 ADMIN = THE_ADMIN # Set in admin.py file
 ADMIN_PASS = THE_ADIM_PASS # Set in admin.py file
 
 SQL_SCHEMA = "setup-schema.sql"
 SQL_DATA = "setup-data.sql"  # Not to be confused with dump file data
-SQL_BACKUP_FILE = "ant-backup.psql" # Backup file with schema information
+SQL_BACKUP_FILE = "antt-backup.psql" # Backup file with schema information
