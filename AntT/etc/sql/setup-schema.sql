d90a75b5bc2c31e8e3e24ffc50cf1ade2f42884e
markroyer <mark.e.royer@gmail.com>
2016-10-26 15:33:45 -0400

    Add site_types and sites tables

--- AntT/etc/sql/setup-schema.sql
+++ AntT/etc/sql/setup-schema.sql
@@ -13,6 +13,10 @@ BEGIN;
 ------------------------------------------------------------------------------
 ------------------------------------------------------------------------------
 
+------------------------------------------------------------------------------
+-- Start Volcano Related Tables
+------------------------------------------------------------------------------
+
 CREATE TABLE countries(
 	country TEXT PRIMARY KEY);
 
@@ -54,6 +58,29 @@ CREATE TABLE volcanoes(
 	FOREIGN KEY (subregion, country) REFERENCES subregions(subregion, country),
 	FOREIGN KEY (region, subregion, country) REFERENCES regions(region, subregion, country));
 	
+------------------------------------------------------------------------------
+-- End Volcano Related Tables
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- Start Sample Related Tables
+------------------------------------------------------------------------------
+
+CREATE TABLE site_types(
+	site_type TEXT PRIMARY KEY);
+	
+CREATE TABLE sites(
+	id TEXT PRIMARY KEY,
+	site_type TEXT REFERENCES site_types(site_type) NOT NULL,
+	latitude REAL NOT NULL CONSTRAINT valid_latitude_range CHECK (-90 <= latitude AND latitude <= 90),
+	longitude REAL NOT NULL CONSTRAINT valid_longitude_range CHECK (-180 <= longitude AND longitude <= 180)
+	elevation_m REAL NOT NULL,
+	comment TEXT);
+
+
+------------------------------------------------------------------------------
+-- End Sample Related Tables
+------------------------------------------------------------------------------
 
 
 ------------------------------------------------------------------------------
