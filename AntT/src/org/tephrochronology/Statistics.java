689353213c5edf86a6fbcc12e26038414400f551
markroyer <mark.e.royer@gmail.com>
2016-11-03 15:51:49 -0400

    Initial commit of statistics class

---
+++ AntT/src/org/tephrochronology/Statistics.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.tephrochronology;
+
+import java.util.List;
+
+/**
+ * A collection of statistical routines for the project.
+ * 
+ * @author Mark Royer
+ *
+ */
+public class Statistics {
+
+	public static double similarityCoefficient(List<Double> xa,
+			List<Double> xb) {
+		return 0;
+	}
+
+	public static double similarityCoefficient(List<Double> xa,
+			List<Double> stda, List<Double> xb, List<Double> stdb) {
+		return 0;
+	}
+}
---
+++ AntT/test/org/tephrochronology/StatisticsTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.tephrochronology;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Mark Royer
+ *
+ */
+public class StatisticsTest {
+
+	/**
+	 * Test method for
+	 * {@link org.tephrochronology.Statistics#similarityCoefficient(java.util.List, java.util.List)}.
+	 */
+	@Test
+	public void testSimilarityCoefficientListOfDoubleListOfDouble() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.tephrochronology.Statistics#similarityCoefficient(java.util.List, java.util.List, java.util.List, java.util.List)}.
+	 */
+	@Test
+	public void testSimilarityCoefficientListOfDoubleListOfDoubleListOfDoubleListOfDouble() {
+		fail("Not yet implemented");
+	}
+
+}
