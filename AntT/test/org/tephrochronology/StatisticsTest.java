186b0fa6d456eef9d77ec32c7d2a59217df4c55d
markroyer <mark.e.royer@gmail.com>
2016-11-08 09:34:27 -0500

    Add initial CSV loading example to test statistics

--- AntT/test/org/tephrochronology/StatisticsTest.java
+++ AntT/test/org/tephrochronology/StatisticsTest.java
@@ -3,15 +3,50 @@
  */
 package org.tephrochronology;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.supercsv.io.CsvListReader;
+import org.supercsv.io.ICsvListReader;
+import org.supercsv.prefs.CsvPreference;
 
 /**
  * @author Mark Royer
  *
  */
 public class StatisticsTest {
+
+	@Before
+	public void setUp() throws Exception {
+		ICsvListReader reader = new CsvListReader(
+				new FileReader(new File("./etc/data/borchardtINAA.csv")),
+				CsvPreference.STANDARD_PREFERENCE);
+
+		String[] header = reader.getHeader(true);
+		List<String> line = reader.read();
+		List<List<Double>> samples = new ArrayList<>();
+		List<List<Double>> sampleStds = new ArrayList<>();
+		while (line != null) {
+			List<Double> newSamples = new ArrayList<>();
+			List<Double> newSampleStds = new ArrayList<>();
+			for (int i = 1; i < line.size() - 1; i += 2) {
+				newSamples.add(Double.valueOf(line.get(i)));
+				newSampleStds.add(line.get(i + 1) != null
+						? Double.valueOf(line.get(i + 1)) : null);
+			}
+			samples.add(newSamples);
+			sampleStds.add(newSampleStds);
+			line = reader.read();
+		}
+
+	}
 
 	/**
 	 * Test method for
@@ -54,17 +89,17 @@ public class StatisticsTest {
 		assertEquals(0.5, Statistics.R(2, 1), 0.001);
 
 	}
-	
+
 	/**
 	 * Test method for
 	 * {@link org.tephrochronology.Statistics#R(double, double)}.
 	 */
-	@Test(expected=IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void testRIllegalException() {
 
 		// Invalid a value
 		Statistics.R(-1, 2);
-		
+
 		Statistics.R(2, -1);
 
 	}
