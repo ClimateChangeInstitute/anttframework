c3272428084d448f1cf69d9639ccf9574a496b19
Willie Stevenson <willie.stevenson@maine.edu>
2016-11-11 16:07:40 -0500

    Add create paths and generate output dir structure

--- AntT/src/org/tephrochronology/Runner.java
+++ AntT/src/org/tephrochronology/Runner.java
@@ -1,5 +1,8 @@
 package org.tephrochronology;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.File;
 
 /**
  * Start the program
@@ -9,21 +12,42 @@ package org.tephrochronology;
  */
 public class Runner {
 
-		
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		final Path PROJECT_ROOT = Paths.get("").toAbsolutePath();
+		final Path SOURCE_DIR = Paths.get(PROJECT_ROOT + "/template");
+
+		// generate directories
+		final Path OUTPUT_DIR = Paths.get(PROJECT_ROOT + "/output");
+		final Path XML_SAMPLES = Paths.get(PROJECT_ROOT + "/output/XMLSamples");
+		final Path SAMPLE_BIA = Paths
+				.get(PROJECT_ROOT + "/output/XMLSamples/BIA");
+		final Path ICE_CORE = Paths
+				.get(PROJECT_ROOT + "/output/XMLSamples/IceCore");
+		final Path AQUATIC_LAKE = Paths
+				.get(PROJECT_ROOT + "/output/XMLSamples/Aquatic/Lake");
+		final Path AQUATIC_MARINE = Paths
+				.get(PROJECT_ROOT + "/output/XMLSamples/Aquatic/Marine");
+		final Path OUTCROP = Paths
+				.get(PROJECT_ROOT + "/output/XMLSamples/Outcrop");
+
 		/*
-		 * Put web layout in WebContent folder
+		 * Put web layout in output folder
 		 */
-		
+		// FileUtils.copyDirectory(SOURCE_DIR.toFile(), OUTPUT_DIR.toFile());
+
 		/*
-		 * Generate all site XMLSamples WebContent/XMLSamples/Samples/SiteFolder/*.xml
+		 * Generate all site XMLSamples
+		 * WebContent/XMLSamples/Samples/SiteFolder/*.xml
 		 */
-		
-		
-		
+
+		OUTPUT_DIR.toFile().mkdir();
+		XML_SAMPLES.toFile().mkdir();
+		SAMPLE_BIA.toFile().mkdir();
+		ICE_CORE.toFile().mkdir();
+		AQUATIC_LAKE.toFile().mkdir();
+		AQUATIC_MARINE.toFile().mkdir();
+		OUTCROP.toFile().mkdir();
+
 	}
-	
-	
-	
+
 }
