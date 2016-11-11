b4c3f031c9f102bb7c7e1722e0689dd1bf3c2a51
markroyer <mark.e.royer@gmail.com>
2016-11-11 11:16:18 -0500

    Add missing equals and hashCode methods

--- AntT/src/org/tephrochronology/model/SubregionID.java
+++ AntT/src/org/tephrochronology/model/SubregionID.java
@@ -3,11 +3,34 @@
  */
 package org.tephrochronology.model;
 
+import java.io.Serializable;
+
 /**
  * @author Mark Royer
  *
  */
-public class SubregionID {
+public class SubregionID implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	String name;
 	String country;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof SubregionID) {
+			SubregionID other = (SubregionID) obj;
+			return name != null && country != null && name.equals(other.name)
+					&& country.equals(other.country);
+		} else
+			return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
 }
