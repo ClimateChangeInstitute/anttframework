1d919ff7b28913774ad986b8b557162994d1dce8
markroyer <mark.e.royer@gmail.com>
2016-11-11 11:08:24 -0500

    Add equals and hashCode methods

--- AntT/src/org/tephrochronology/model/RegionID.java
+++ AntT/src/org/tephrochronology/model/RegionID.java
@@ -3,11 +3,35 @@
  */
 package org.tephrochronology.model;
 
+import java.io.Serializable;
+
 /**
  * @author Mark Royer
  *
  */
-public class RegionID {
+public class RegionID implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	String name;
 	SubregionID subRegion;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof RegionID) {
+			RegionID other = (RegionID) obj;
+			return name != null && subRegion != null && name.equals(other)
+					&& subRegion.equals(other.subRegion);
+		} else
+			return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
 }
