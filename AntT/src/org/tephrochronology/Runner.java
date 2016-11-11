3ba146602ad680b4ac4ac2871ee43b98a135777d
Willie Stevenson <willie.stevenson@maine.edu>
2016-11-11 15:31:03 -0500

    Add site generator class

---
+++ AntT/src/org/tephrochronology/Runner.java
@@ -0,0 +1,29 @@
+package org.tephrochronology;
+
+
+/**
+ * Start the program
+ * 
+ * @author Willie Stevenson
+ *
+ */
+public class Runner {
+
+		
+	public static void main(String[] args)
+	{
+		/*
+		 * Put web layout in WebContent folder
+		 */
+		
+		/*
+		 * Generate all site XMLSamples WebContent/XMLSamples/Samples/SiteFolder/*.xml
+		 */
+		
+		
+		
+	}
+	
+	
+	
+}
