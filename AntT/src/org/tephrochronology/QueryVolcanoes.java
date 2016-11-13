51decfd23ff7f5c57af60d97aa4974976509974a
markroyer <mark.e.royer@gmail.com>
2016-11-13 15:34:59 -0500

    Extract database properties to defined class

--- AntT/src/org/tephrochronology/QueryVolcanoes.java
+++ AntT/src/org/tephrochronology/QueryVolcanoes.java
@@ -3,31 +3,17 @@
  */
 package org.tephrochronology;
 
-import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_DRIVER;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_PASSWORD;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_URL;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_USER;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.LOGGING_LEVEL;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.LOGGING_SESSION;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.LOGGING_THREAD;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.LOGGING_TIMESTAMP;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.TARGET_SERVER;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.TRANSACTION_TYPE;
-import static org.eclipse.persistence.logging.SessionLog.OFF_LABEL;
+import static org.tephrochronology.DBProperties.setupProperties;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-import javax.persistence.spi.PersistenceUnitTransactionType;
 
-import org.eclipse.persistence.config.TargetServer;
 import org.tephrochronology.model.Instrument;
 import org.tephrochronology.model.OutcropSample;
 import org.tephrochronology.model.Ref;
@@ -59,12 +45,7 @@ public class QueryVolcanoes {
 
 		em.getTransaction().begin();
 
-		TypedQuery<Volcano> q = em.createQuery("SELECT v FROM Volcano v",
-				Volcano.class);
-
-		List<Volcano> vs = q.getResultList();
-
-		// vs.stream().forEach(v -> System.out.println(v.getName()));
+		// queryVolcanoesExample(em);
 
 		TypedQuery<Ref> refQ = em.createQuery("SELECT r FROM Ref r", Ref.class);
 
@@ -73,20 +54,14 @@ public class QueryVolcanoes {
 		refs.stream().forEach(r -> System.out
 				.println(r.getDoi() + " " + r.getGrainSizes().size()));
 
-//		OutcropSample os = new OutcropSample("sample1", "long sample1",
-//				"test sampled", LocalDate.now(), "Just some test comments",
-//				new Site("site1", new SiteType("site1Type"), 70, 50, 30,
-//						"first site comment"),
-//				new Instrument("inst1", "inst1 long", "inst1 loc",
-//						"inst1 comment"),
-//				new ArrayList<>(), new ArrayList<>(), 210010);
-//		
-//		em.persist(os);
+		// addOutcropExample(em);
 
-		TypedQuery<OutcropSample> osQ = em.createQuery("SELECT o FROM OutcropSample o WHERE o.volcanoNumber = 210010", OutcropSample.class);
+		TypedQuery<OutcropSample> osQ = em.createQuery(
+				"SELECT o FROM OutcropSample o WHERE o.volcanoNumber = 210010",
+				OutcropSample.class);
 
 		System.out.println(osQ.getSingleResult().getSampleID());
-		
+
 		em.getTransaction().commit();
 
 		em.close();
@@ -94,31 +69,31 @@ public class QueryVolcanoes {
 
 	}
 
-	private static Map<String, String> setupProperties(String[] args) {
+	/**
+	 * @param em
+	 */
+	static void queryVolcanoesExample(EntityManager em) {
+		TypedQuery<Volcano> q = em.createQuery("SELECT v FROM Volcano v",
+				Volcano.class);
 
-		Map<String, String> properties = new HashMap<>();
+		List<Volcano> vs = q.getResultList();
 
-		// Ensure RESOURCE_LOCAL transactions is used.
-		properties.put(TRANSACTION_TYPE,
-				PersistenceUnitTransactionType.RESOURCE_LOCAL.name());
+		vs.stream().forEach(v -> System.out.println(v.getName()));
+	}
 
-		// Configure the internal EclipseLink connection pool
-		properties.put(JDBC_DRIVER, "org.postgresql.Driver");
-		properties.put(JDBC_URL, "jdbc:postgresql://localhost:5432/antt");
-		properties.put(JDBC_USER, args[0]);
-		properties.put(JDBC_PASSWORD, args[1]);
+	/**
+	 * @param em
+	 */
+	static void addOutcropExample(EntityManager em) {
+		OutcropSample os = new OutcropSample("sample1", "long sample1",
+				"test sampled", LocalDate.now(), "Just some test comments",
+				new Site("site1", new SiteType("site1Type"), 70, 50, 30,
+						"first site comment"),
+				new Instrument("inst1", "inst1 long", "inst1 loc",
+						"inst1 comment"),
+				new ArrayList<>(), new ArrayList<>(), 210010);
 
-		// Configure logging. FINE ensures all SQL is shown
-		properties.put(LOGGING_LEVEL, OFF_LABEL);
-		properties.put(LOGGING_TIMESTAMP, "false");
-		properties.put(LOGGING_THREAD, "false");
-		properties.put(LOGGING_SESSION, "false");
-
-		// Ensure that no server-platform is configured
-		properties.put(TARGET_SERVER, TargetServer.None);
-
-		return properties;
-
+		em.persist(os);
 	}
 
 }
