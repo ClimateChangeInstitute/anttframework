8b35b2f0e578cfcb40ef7bd1302496be288ed660
markroyer <mark.e.royer@gmail.com>
2016-11-13 21:59:43 -0500

    Make MethodType a DB entity

--- AntT/src/org/tephrochronology/model/MethodType.java
+++ AntT/src/org/tephrochronology/model/MethodType.java
@@ -3,13 +3,32 @@
  */
 package org.tephrochronology.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Mark Royer
  *
  */
-public class MethodType {
+@Entity
+@Table(name = "method_types")
+public class MethodType implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="method_type")
 	private String type;
+
+	public MethodType() {
+	}
 
 	/**
 	 * @param type
