5b1d949b5adc6cb14c8a292c88886c1efad047e3
Willie Stevenson <willie.stevenson@maine.edu>
2016-12-15 20:02:35 -0500

    Remove all console.log output in js files

--- AntT/WebContent/js/module.js
+++ AntT/WebContent/js/module.js
@@ -18,11 +18,8 @@ app.controller('team', function($scope, dataSource) {
 	$scope.AppController = [];
 	
 	dataSource.getMembers().success(function(data) {
-		console.log(data);
 		var x2js = new X2JS();
 		var json = x2js.xml_str2json(data);
-		console.log(json);
-		console.log(json.team.member);
 		$scope.dataSet = json.team.member;
 	});
 });
@@ -31,11 +28,8 @@ app.controller('methodology', function($scope, dataSource) {
 	$scope.AppController = [];
 	
 	dataSource.getPapers().success(function(data) {
-		console.log(data);
 		var x2js = new X2JS();
 		var json = x2js.xml_str2json(data);
-		console.log(json);
-		console.log(json.methodology.paper);
 		$scope.dataSet = json.methodology.paper;
 	});
 });
