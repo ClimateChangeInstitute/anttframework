1ea4b277d19cd7761ec4edcd5a773192aa1fdda2
markroyer <mark.e.royer@gmail.com>
2016-12-29 15:23:50 -0500

    Make images written to individual files

--- AntT/src/org/tephrochronology/DBExporter.java
+++ AntT/src/org/tephrochronology/DBExporter.java
@@ -8,18 +8,22 @@ import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_USER
 import static org.tephrochronology.DBProperties.setupProperties;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
 import java.util.Scanner;
 
+import javax.imageio.ImageIO;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import org.tephrochronology.model.Image;
 
 /**
  * A collection of functions for saving database objects as nicely structured
@@ -90,9 +94,6 @@ public class DBExporter {
 
 		// Class.forName("org.postgresql.Driver");
 		//
-		// EntityManagerFactory emf = Persistence
-		// .createEntityManagerFactory(dbName, props);
-		// EntityManager em = emf.createEntityManager();
 		//
 		// Connection conn = DriverManager.getConnection(
 		// "jdbc:postgresql://localhost:5432/" + dbName,
@@ -104,7 +105,7 @@ public class DBExporter {
 		copyTableToFile(dbName, dir, "volcano_types");
 		copyTableToFile(dbName, dir, "rock_types");
 		copyTableToFile(dbName, dir, "tectonic_settings");
-		
+
 		copyTableToFile(dbName, dir, "volcanoes");
 
 		copyTableToFile(dbName, dir, "site_types");
@@ -113,16 +114,41 @@ public class DBExporter {
 
 		copyTableToFile(dbName, dir, "refs");
 
-		copyTableToFile(dbName, dir, "samples");
 		copyTableToFile(dbName, dir, "samples_refs");
 
 		copyTableToFile(dbName, dir, "grain_sizes");
 		copyTableToFile(dbName, dir, "grain_sizes_refs");
 
 		// TODO write out images to jpg files
-//		copyTableToFile(dbName, dir, "images");
-//		copyTableToFile(dbName, dir, "images_image_id_seq");
+		// copyTableToFile(dbName, dir, "images");
+
+		File imageDir = new File(dir, "images");
+		if (!imageDir.exists())
+			imageDir.mkdir();
+
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory(dbName, props);
+		EntityManager em = emf.createEntityManager();
+
+		TypedQuery<Image> images = em.createQuery("SELECT i FROM Image i",
+				Image.class);
+
+		for (Image i : images.getResultList()) {
+			try (FileOutputStream io = new FileOutputStream(
+					new File(imageDir, i.getImageID()))) {
+				io.write(i.getBytes());
+				io.flush();
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw e;
+			}
+		}
+
 		copyTableToFile(dbName, dir, "samples_images");
+
+		// TODO combine samples data for each type no need to write samples
+		// table
+		copyTableToFile(dbName, dir, "samples");
 
 		copyTableToFile(dbName, dir, "icecore_samples");
 		copyTableToFile(dbName, dir, "bia_samples");
@@ -139,7 +165,6 @@ public class DBExporter {
 		copyTableToFile(dbName, dir, "elements");
 		copyTableToFile(dbName, dir, "units");
 		copyTableToFile(dbName, dir, "mm_elements_data");
-		
 
 	}
 
