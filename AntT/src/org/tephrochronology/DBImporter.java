95560ce931ede8ab5788f59402005c99cc7bc04e
markroyer <mark.e.royer@gmail.com>
2016-12-31 17:37:08 -0500

    Add import outline

--- AntT/src/org/tephrochronology/DBImporter.java
+++ AntT/src/org/tephrochronology/DBImporter.java
@@ -6,8 +6,13 @@ package org.tephrochronology;
 import static org.tephrochronology.DBProperties.dBExists;
 import static org.tephrochronology.DBProperties.setupProperties;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A collection of functions to import data to the database from a collection of
@@ -37,9 +42,24 @@ public class DBImporter {
 
 		Map<String, String> properties = setupProperties(args);
 
-		String dbName = "antt";
+		String dbName = DBProperties.getDB(properties);
 
 		File dir = new File(String.format("%s-data", dbName));
+
+		DBImporter dbi = new DBImporter(properties);
+
+		dbi.importData(dir);
+
+	}
+
+	/**
+	 * 
+	 * @param dir
+	 * @throws IOException
+	 */
+	public void importData(File dir) throws IOException {
+
+		String dbName = DBProperties.getDB(props);
 
 		if (!dir.exists()) {
 			System.out.printf(
@@ -48,14 +68,63 @@ public class DBImporter {
 					dir.getAbsolutePath());
 		} else {
 
-			if (dBExists(properties)) {
-				// Drop and create from scratch for now
+			if (dBExists(props)) {
+				// Delete the database
+				dropDatabase();
 			} else {
-				// run the setup script and populate from ant-data folder
-				
-				DBImporter dbi = new DBImporter(properties);
+				// Run the setup script schema only
+				System.out.printf("The %s DB does not exist.  Creating...",
+						dbName);
 
+				// TODO create database 
+				
 			}
+			
+			// TODO populate the datbase from ant-data folder
+
+		}
+
+	}
+
+	/**
+	 * Delete the database using the python script.
+	 * 
+	 * @throws IOException
+	 */
+	private void dropDatabase() throws IOException {
+		// The -u option is critical to make Python flush buffers for
+		// stdin and stdout!
+		ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c",
+				"python -u ./etc/sql/teardown.py");
+
+		pb.redirectErrorStream(true);
+
+		Process p = pb.start();
+
+		BufferedReader input = new BufferedReader(
+				new InputStreamReader(p.getInputStream()));
+
+		PrintWriter out = new PrintWriter(p.getOutputStream());
+
+		out.println("y");
+		out.flush();
+
+		String line = null;
+		while ((line = input.readLine()) != null) {
+			System.out.println(line);
+		}
+
+		input.close();
+
+		try {
+			p.waitFor(10, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			throw new IOException(
+					"Failed to finish in allotted timeout period.");
+		}
+
+		if (p.exitValue() != 0) {
+			throw new IOException("Exited with nonzero value.");
 		}
 	}
 
