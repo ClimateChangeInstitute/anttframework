8dcb4d380c0770c30ba98b7a6deb183d30693919
markroyer <mark.e.royer@gmail.com>
2017-01-07 16:07:47 -0500

    Add root category Java class

---
+++ AntT/src/org/tephrochronology/model/Category.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package org.tephrochronology.model;
+
+import static javax.persistence.DiscriminatorType.CHAR;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+/**
+ * Represents a collection of samples, for example, an ice core.
+ * 
+ * @author Mark Royer
+ *
+ */
+@Entity
+@Table(name = "categories")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "category_type", discriminatorType = CHAR)
+public abstract class Category implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="category_id")
+	private String categoryID;
+	
+	@JoinColumn(name = "site_id")
+	private Site site;
+	
+	public Category(){
+	}
+
+	/**
+	 * @param categoryID
+	 * @param site
+	 */
+	public Category(String categoryID, Site site) {
+		super();
+		this.categoryID = categoryID;
+		this.site = site;
+	}
+
+	public String getCategoryID() {
+		return categoryID;
+	}
+
+	public void setCategoryID(String categoryID) {
+		this.categoryID = categoryID;
+	}
+
+	public Site getSite() {
+		return site;
+	}
+
+	public void setSite(Site site) {
+		this.site = site;
+	}
+
+}
