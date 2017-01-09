f590025e0940b690e724d9ab0c467954512497d8
Willie Stevenson <willie.stevenson@maine.edu>
2017-01-09 16:33:47 -0500

    Separate primary geochemical data from secondary geochemical data on the fly and display

--- AntT/WebContent/js/results.module.js
+++ AntT/WebContent/js/results.module.js
@@ -50,6 +50,48 @@ app.controller('results', function($scope, dataSource) {
 					return val;
 				}), antt.valuesArray(Object.keys(s), e));
 
+				e.primaryElementData = [];
+				e.secondaryElementData = [];
+
+				$.each(e.elementData, function(i, val) {
+					if (val.symbol == "SiO2") {
+						val.order = 1;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "TiO2") {
+						val.order = 2;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "Al2O3") {
+						val.order = 3;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "TiO2") {
+						val.order = 4;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "FeO") {
+						val.order = 5;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "MnO") {
+						val.order = 6;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "MgO") {
+						val.order = 7;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "CaO") {
+						val.order = 8;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "Na2O") {
+						val.order = 9;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "K2O") {
+						val.order = 10;
+						e.primaryElementData.push(val);
+					} else if(val.symbol == "P2O5") {
+						val.order = 11;
+						e.primaryElementData.push(val);
+					} else {
+						e.secondaryElementData.push(val);
+					}
+				});
+
 				searchRes.push({
 					simVal : simVal,
 					mme : e
@@ -73,12 +115,13 @@ app.controller('results', function($scope, dataSource) {
 				searchQuery : searchQuery,
 				count : i
 			});
+			
 		});
+
+		console.log(allResults);
 
 		// reverse array so most recent search is displayed first
 		allResults.reverse();
-
-		console.log(allResults);
 
 		$scope.filtered = allResults;
 		$scope.$apply();
