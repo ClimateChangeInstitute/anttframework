7e8340c2670caea507209f49442659a6bedbb987
markroyer <mark.e.royer@gmail.com>
2017-01-10 00:38:36 -0500

    Simplify primary element ordering mechanism

--- AntT/WebContent/js/results.module.js
+++ AntT/WebContent/js/results.module.js
@@ -20,6 +20,11 @@ app.controller('results', function($scope, dataSource) {
 
 	$scope.AppController = [];
 
+	// These are the Elements that will presented in the primary section.
+	// They will appear in the order listed in this array.
+	var primaryElementOrder = [ "SiO2", "TiO2", "Al2O3", "TiO2", "FeO", "MnO",
+			"MgO", "CaO", "Na2O", "K2O", "P2O5" ];
+
 	// If we decide not to use default behavior, let's use this alternative
 	// behavior
 	// $scope.delay = 0;
@@ -29,99 +34,75 @@ app.controller('results', function($scope, dataSource) {
 	// $scope.backdrop = true;
 	// $scope.promise = null;
 
-	$scope.promise = antt.loadMMElements('generated/allMMElements.xml', function(
-			allMMElements) {
+	$scope.promise = antt.loadMMElements('generated/allMMElements.xml',
+			function(allMMElements) {
 
-		var values = antt.getUrlParameters();
-		var searches = antt.getQueriedElements(values, 's');
+				var values = antt.getUrlParameters();
+				var searches = antt.getQueriedElements(values, 's');
 
-		var allResults = [];
+				var allResults = [];
 
-		$.each(searches, function(i, s) {
+				$.each(searches, function(i, s) {
 
-			var filtered = antt.filterMMElements(Object.keys(s), allMMElements, '%');
+					var filtered = antt.filterMMElements(Object.keys(s),
+							allMMElements, '%');
 
-			var searchRes = [];
+					var searchRes = [];
 
-			$.each(filtered, function(i, e) {
+					$.each(filtered,
+							function(i, e) {
 
-				var simVal = antt.statistics.similarityCoefficientListList($.map(s, function(
-						val, key) {
-					return val;
-				}), antt.valuesArray(Object.keys(s), e));
+								var simVal = antt.statistics
+										.similarityCoefficientListList($.map(s,
+												function(val, key) {
+													return val;
+												}), antt.valuesArray(Object
+												.keys(s), e));
 
-				e.primaryElementData = [];
-				e.secondaryElementData = [];
+								e.primaryElementData = [];
+								e.secondaryElementData = [];
 
-				$.each(e.elementData, function(i, val) {
-					if (val.symbol == "SiO2") {
-						val.order = 1;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "TiO2") {
-						val.order = 2;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "Al2O3") {
-						val.order = 3;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "TiO2") {
-						val.order = 4;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "FeO") {
-						val.order = 5;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "MnO") {
-						val.order = 6;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "MgO") {
-						val.order = 7;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "CaO") {
-						val.order = 8;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "Na2O") {
-						val.order = 9;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "K2O") {
-						val.order = 10;
-						e.primaryElementData.push(val);
-					} else if(val.symbol == "P2O5") {
-						val.order = 11;
-						e.primaryElementData.push(val);
-					} else {
-						e.secondaryElementData.push(val);
-					}
+								$.each(e.elementData, function(i, val) {
+									var i = primaryElementOrder
+											.indexOf(val.symbol);
+									if (i >= 0) {
+										val.order = i;
+										e.primaryElementData.push(val);
+									} else {
+										e.secondaryElementData.push(val);
+									}
+								});
+
+								searchRes.push({
+									simVal : simVal,
+									mme : e
+								});
+							});
+
+					searchRes.sort(function(a, b) {
+						return b.simVal - a.simVal;
+					});
+
+					searchQuery = "";
+
+					$.each(s, function(a, b) {
+						searchQuery += a + " = " + b + ",";
+					});
+
+					searchQuery = searchQuery.slice(0, -1);
+
+					allResults.push({
+						searchRes : searchRes,
+						searchQuery : searchQuery,
+						count : i
+					});
+
 				});
 
-				searchRes.push({
-					simVal : simVal,
-					mme : e
-				});
+				// reverse array so most recent search is displayed first
+				allResults.reverse();
+
+				$scope.filtered = allResults;
+				$scope.$apply();
 			});
-
-			searchRes.sort(function(a, b) {
-				return b.simVal - a.simVal;
-			});
-
-			searchQuery = "";
-
-			$.each(s, function(a, b) {
-				searchQuery += a + " = " + b + ",";
-			});
-
-			searchQuery = searchQuery.slice(0, -1);
-
-			allResults.push({
-				searchRes : searchRes,
-				searchQuery : searchQuery,
-				count : i
-			});
-			
-		});
-
-		// reverse array so most recent search is displayed first
-		allResults.reverse();
-
-		$scope.filtered = allResults;
-		$scope.$apply();
-	});
 });
