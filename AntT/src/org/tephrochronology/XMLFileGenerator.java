aca419d22e3cb7f86042e474846742c613f336a9
markroyer <mark.e.royer@gmail.com>
2017-01-13 14:05:23 -0500

    Fix XMLFileGenerator imports

--- AntT/src/org/tephrochronology/XMLFileGenerator.java
+++ AntT/src/org/tephrochronology/XMLFileGenerator.java
@@ -6,7 +6,6 @@ package org.tephrochronology;
 import static org.tephrochronology.DBProperties.setupProperties;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
