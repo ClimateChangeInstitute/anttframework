c3bca6a841e38c262201a4053907d038737286fc
markroyer <mark.e.royer@gmail.com>
2017-01-31 15:34:03 -0500

    Confirm overwrite of existing generated folder

--- AntT/src/org/tephrochronology/StaticSiteDataGenerator.java
+++ AntT/src/org/tephrochronology/StaticSiteDataGenerator.java
@@ -5,10 +5,12 @@ import static org.tephrochronology.DBProperties.DEFAULT_PASSWORD_FILE;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.PropertyException;
 
+import org.apache.commons.io.FileUtils;
 import org.tephrochronology.model.BIASample;
 import org.tephrochronology.model.IceCoreSample;
 import org.tephrochronology.model.LakeSample;
@@ -43,8 +45,27 @@ public class StaticSiteDataGenerator {
 		final Path WEBCONTENT_DIR = Paths.get(PROJECT_ROOT + "/WebContent");
 
 		// generate directories
-		final Path GEN_DIR = appendChildAndCreateDir(WEBCONTENT_DIR,
-				"/generated");
+		Path GEN_DIR = Paths.get(WEBCONTENT_DIR + "/generated");
+
+		if (!GEN_DIR.toFile().mkdirs()) {
+			System.out.printf(
+					"The folder '%s' exists.\n"
+							+ "The contents will be deleted and the folder recreated.\n"
+							+ "Are you sure that you want to continue? (y/N).",
+					GEN_DIR);
+			try (Scanner in = new Scanner(System.in)) {
+				if (!"y".equalsIgnoreCase(in.nextLine())) {
+					System.out.println(
+							"Okay; I won't overwrite the existing data.");
+					System.exit(0);
+				} else {
+					FileUtils.deleteDirectory(GEN_DIR.toFile());
+					GEN_DIR = appendChildAndCreateDir(WEBCONTENT_DIR,
+							"/generated");
+				}
+			}
+		}
+
 		final Path XML_SAMPLES = appendChildAndCreateDir(GEN_DIR,
 				"/XMLSamples");
 		final Path SAMPLE_BIA = appendChildAndCreateDir(XML_SAMPLES, "/BIA");
