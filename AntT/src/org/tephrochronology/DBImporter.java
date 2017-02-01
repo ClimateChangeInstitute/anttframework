ccbb78b9087ad0ad487e1e0fa2a546e211556e55
markroyer <mark.e.royer@gmail.com>
2017-01-31 21:04:06 -0500

    Add append or recreate DB for importer

--- AntT/src/org/tephrochronology/DBImporter.java
+++ AntT/src/org/tephrochronology/DBImporter.java
@@ -3,8 +3,6 @@
  */
 package org.tephrochronology;
 
-import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_PASSWORD;
-import static org.eclipse.persistence.config.PersistenceUnitProperties.JDBC_USER;
 import static org.tephrochronology.DBProperties.DEFAULT_PASSWORD_FILE;
 import static org.tephrochronology.DBProperties.dBExists;
 import static org.tephrochronology.DBProperties.setupProperties;
@@ -12,6 +10,7 @@ import static org.tephrochronology.DBProperties.setupProperties;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -30,6 +29,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.postgresql.copy.CopyManager;
+import org.postgresql.core.BaseConnection;
 import org.tephrochronology.model.Image;
 
 /**
@@ -92,27 +93,52 @@ public class DBImporter {
 					"The folder '%s' does not exist.  "
 							+ "Please make sure that your data can be found at that location.",
 					dataDir.getAbsolutePath());
+			System.out.println("Data has not been imported.");
+			System.exit(0);
 		} else {
 
 			if (dBExists(props)) {
 
 				System.out.printf(
-						"The '%s' database exists and the contents will be overwritten.\n"
-								+ "Data found in the '%s' directory will be used.  Continue? (y/N):",
-						dbName, dataDir.getAbsolutePath());
+						"The '%s' database exists. Append to existing data tables? (Y/n):",
+						dbName);
+
 				Scanner scan = new Scanner(System.in);
+
 				String ans = scan.nextLine();
-				scan.close();
-				if (!"y".equalsIgnoreCase(ans)) {
-					System.out.println("Exiting");
-					System.exit(-1);
-				} else {
-					dropDatabase();
+
+				boolean append = true;
+				if (!"".equals(ans) && !"y".equalsIgnoreCase(ans)) {
+					append = false;
 				}
 
+				System.out.printf(
+						"You have chosen to "
+								+ (append ? "append to the database.\n"
+										: "recreate the database. IMPORTANT: The current database will be deleted!\n")
+								+ "Data found in the '%s' directory will be used.  Continue? (y/N):",
+						dataDir.getAbsolutePath());
+
+				ans = scan.nextLine();
+
+				scan.close();
+
+				if (!"y".equalsIgnoreCase(ans)) {
+					System.out.println("Exiting");
+					System.exit(0);
+				}
+
+				if (!append) { // Recreate database...
+					dropDatabase();
+					createDatabase();
+				}
+
+			} else {
+				System.out.println(
+						"The database does not exist.  It will be created.");
+				createDatabase();
 			}
 
-			createDatabase();
 			importFiles(dataDir, dbName);
 
 		}
@@ -128,121 +154,131 @@ public class DBImporter {
 	 *            The name of the database to import to (Not null)
 	 * @throws IOException
 	 *             Thrown if unable to read from files in the specified folder
-	 * @throws SQLException
-	 * @throws ClassNotFoundException
+	 * 
 	 */
-	private void importFiles(File dataDir, String dbName)
-			throws IOException, SQLException, ClassNotFoundException {
+	private void importFiles(File dataDir, String dbName) {
 
-		copyFileToTable(dbName, "countries", dataDir);
-		copyFileToTable(dbName, "subregions", dataDir);
-		copyFileToTable(dbName, "regions", dataDir);
-		copyFileToTable(dbName, "volcano_types", dataDir);
-		copyFileToTable(dbName, "rock_types", dataDir);
-		copyFileToTable(dbName, "tectonic_settings", dataDir);
+		try (Connection conn = DBProperties.getJDBCConnection(props)) {
 
-		copyFileToTable(dbName, "volcanoes", dataDir);
+			conn.setAutoCommit(false); // Begin transaction
 
-		copyFileToTable(dbName, "sites", dataDir);
+			copyFileToTable(conn, dataDir, "countries");
+			copyFileToTable(conn, dataDir, "subregions");
+			copyFileToTable(conn, dataDir, "regions");
+			copyFileToTable(conn, dataDir, "volcano_types");
+			copyFileToTable(conn, dataDir, "rock_types");
+			copyFileToTable(conn, dataDir, "tectonic_settings");
 
-		copyFileToTable(dbName, "areas", dataDir);
-		copyFileToTable(dbName, "areas_sites", dataDir);
+			copyFileToTable(conn, dataDir, "volcanoes");
 
-		copyFileToTable(dbName, "instruments", dataDir);
+			copyFileToTable(conn, dataDir, "sites");
 
-		copyFileToTable(dbName, "refs", dataDir);
+			copyFileToTable(conn, dataDir, "areas");
+			copyFileToTable(conn, dataDir, "areas_sites");
 
-		importImageFiles(dataDir);
+			copyFileToTable(conn, dataDir, "instruments");
 
-		Connection conn = DBProperties.getJDBCConnection(props);
+			copyFileToTable(conn, dataDir, "refs");
 
-		Statement st = conn.createStatement();
+			importImageFiles(dataDir);
 
-		// @formatter:off
-		st.execute("CREATE TABLE images_tmp("
-				 + "image_id TEXT PRIMARY KEY, "
-				 + "comments TEXT)");
-		// @formatter:on
+			Statement st = conn.createStatement();
 
-		copyFileToTable(dbName, "images_tmp", new File(dataDir, "images.csv"),
-				dataDir);
+			// @formatter:off
+			st.execute("CREATE TABLE images_tmp("
+					 + "image_id TEXT PRIMARY KEY, "
+					 + "comments TEXT)");
+			// @formatter:on
 
-		// @formatter:off
-		st.executeUpdate(
-				  "UPDATE images "
-				+ "SET comments = t.comments "
-				+ "FROM images_tmp t "
-				+ "WHERE images.image_id = t.image_id");
-		// @formatter:on
+			copyFileToTable2(conn, "images_tmp",
+					new File(dataDir, "images.csv"));
 
-		st.executeUpdate("DROP TABLE images_tmp");
+			// @formatter:off
+			st.executeUpdate(
+					  "UPDATE images "
+					+ "SET comments = t.comments "
+					+ "FROM images_tmp t "
+					+ "WHERE images.image_id = t.image_id");
+			// @formatter:on
 
-		loadCategoryData(dataDir, dbName, "icecore_categories",
-				"drilled_by TEXT, drilling_dates TEXT,core_diameter TEXT,max_core_depth TEXT,core_age_range TEXT",
-				"I",
-				"category_id,drilled_by,drilling_dates,core_diameter,max_core_depth,core_age_range",
-				conn);
+			st.executeUpdate("DROP TABLE images_tmp");
 
-		loadSampleData(dataDir, dbName, "icecore_samples",
-				"volcano_number INTEGER,topdepth_m REAL,bottomdepth_m REAL,topyear_bp REAL,bottomyear_bp REAL",
-				"I",
-				"sample_id,volcano_number,topdepth_m,bottomdepth_m,topyear_bp,bottomyear_bp",
-				conn);
+			loadCategoryData(dataDir, "icecore_categories",
+					"drilled_by TEXT, drilling_dates TEXT,core_diameter TEXT,max_core_depth TEXT,core_age_range TEXT",
+					"I",
+					"category_id,drilled_by,drilling_dates,core_diameter,max_core_depth,core_age_range",
+					conn);
 
-		loadCategoryData(dataDir, dbName, "bia_categories",
-				"deep TEXT,thickness_cm TEXT,trend TEXT", "B",
-				"category_id,deep,thickness_cm,trend", conn);
+			loadSampleData(dataDir, "icecore_samples",
+					"volcano_number INTEGER,topdepth_m REAL,bottomdepth_m REAL,topyear_bp REAL,bottomyear_bp REAL",
+					"I",
+					"sample_id,volcano_number,topdepth_m,bottomdepth_m,topyear_bp,bottomyear_bp",
+					conn);
 
-		loadSampleData(dataDir, dbName, "bia_samples",
-				"volcano_number INTEGER ", "B", "sample_id, volcano_number ",
-				conn);
+			loadCategoryData(dataDir, "bia_categories",
+					"deep TEXT,thickness_cm TEXT,trend TEXT", "B",
+					"category_id,deep,thickness_cm,trend", conn);
 
-		copyFileToTable(dbName, "corer_types", dataDir);
+			loadSampleData(dataDir, "bia_samples", "volcano_number INTEGER ",
+					"B", "sample_id, volcano_number ", conn);
 
-		copyFileToTable(dbName, "method_types", dataDir);
+			copyFileToTable(conn, dataDir, "corer_types");
 
-		loadCategoryData(dataDir, dbName, "lake_categories",
-				"corer_type TEXT,age TEXT,core_length_m REAL,collection_date DATE",
-				"L", "category_id,corer_type,age,core_length_m,collection_date",
-				conn);
+			copyFileToTable(conn, dataDir, "method_types");
 
-		loadSampleData(dataDir, dbName, "lake_samples",
-				"volcano_number INTEGER,depth_m REAL, thickness_cm REAL", "L",
-				"sample_id, volcano_number, depth_m, thickness_cm", conn);
+			loadCategoryData(dataDir, "lake_categories",
+					"corer_type TEXT,age TEXT,core_length_m REAL,collection_date DATE",
+					"L",
+					"category_id,corer_type,age,core_length_m,collection_date",
+					conn);
 
-		loadCategoryData(dataDir, dbName, "marine_categories",
-				"corer_type TEXT,age TEXT,core_length_m REAL,collection_date DATE",
-				"M", "category_id,corer_type,age,core_length_m,collection_date",
-				conn);
+			loadSampleData(dataDir, "lake_samples",
+					"volcano_number INTEGER,depth_m REAL, thickness_cm REAL",
+					"L", "sample_id, volcano_number, depth_m, thickness_cm",
+					conn);
 
-		loadSampleData(dataDir, dbName, "marine_samples",
-				"volcano_number INTEGER, depth_m REAL, thickness_cm REAL", "M",
-				"sample_id, volcano_number, depth_m, thickness_cm", conn);
+			loadCategoryData(dataDir, "marine_categories",
+					"corer_type TEXT,age TEXT,core_length_m REAL,collection_date DATE",
+					"M",
+					"category_id,corer_type,age,core_length_m,collection_date",
+					conn);
 
-		loadCategoryData(dataDir, dbName, "outcrop_categories", "", "O",
-				"category_id", conn);
+			loadSampleData(dataDir, "marine_samples",
+					"volcano_number INTEGER, depth_m REAL, thickness_cm REAL",
+					"M", "sample_id, volcano_number, depth_m, thickness_cm",
+					conn);
 
-		loadSampleData(dataDir, dbName, "outcrop_samples",
-				"volcano_number INTEGER", "O", "sample_id, volcano_number",
-				conn);
+			loadCategoryData(dataDir, "outcrop_categories", "", "O",
+					"category_id", conn);
 
-		copyFileToTable(dbName, "samples_images", dataDir);
+			loadSampleData(dataDir, "outcrop_samples", "volcano_number INTEGER",
+					"O", "sample_id, volcano_number", conn);
 
-		copyFileToTable(dbName, "samples_refs", dataDir);
+			copyFileToTable(conn, dataDir, "samples_images");
 
-		copyFileToTable(dbName, "grain_sizes", dataDir);
-		copyFileToTable(dbName, "grain_sizes_refs", dataDir);
+			copyFileToTable(conn, dataDir, "samples_refs");
 
-		copyFileToTable(dbName, "mm_elements", dataDir);
+			copyFileToTable(conn, dataDir, "grain_sizes");
+			copyFileToTable(conn, dataDir, "grain_sizes_refs");
 
-		copyFileToTable(dbName, "chemistries", dataDir);
-		copyFileToTable(dbName, "units", dataDir);
+			copyFileToTable(conn, dataDir, "mm_elements");
 
-		copyFileToTable(dbName, "mm_elements_data", dataDir);
+			copyFileToTable(conn, dataDir, "chemistries");
+			copyFileToTable(conn, dataDir, "units");
 
-		System.out.printf(
-				"File import completed.  Data can be found in the '%s' database.\n",
-				dbName);
+			copyFileToTable(conn, dataDir, "mm_elements_data");
+
+			conn.commit();
+
+			System.out.printf(
+					"File import completed.  Data can be found in the '%s' database.\n",
+					dbName);
+
+		} catch (SQLException | IOException | ClassNotFoundException e) {
+			System.err.println(e.getMessage());
+			System.err.println(
+					"Unable to import data.  The reason is specified above.");
+		}
 	}
 
 	/**
@@ -252,7 +288,7 @@ public class DBImporter {
 	 * @throws SQLException
 	 * @throws IOException
 	 */
-	private void loadCategoryData(File dataDir, String dbName, String tableName,
+	private void loadCategoryData(File dataDir, String tableName,
 			String extraColumnsWithTypes, String typeChar, String extraColumns,
 			Connection conn) throws SQLException, IOException {
 
@@ -264,11 +300,9 @@ public class DBImporter {
 				"CREATE TABLE %s(category_id TEXT PRIMARY KEY,site_id TEXT ",
 				tmpTable) + (extraColumnsWithTypes.isEmpty() ? "" : ", ")
 				+ extraColumnsWithTypes + ")";
-		System.out.println(str);
 		st.execute(str);
 
-		copyFileToTable(dbName, tmpTable, new File(dataDir, tableName + ".csv"),
-				dataDir);
+		copyFileToTable2(conn, tmpTable, new File(dataDir, tableName + ".csv"));
 
 		st.executeUpdate(String.format(
 				"INSERT INTO categories (category_id, site_id, category_type) "
@@ -288,7 +322,7 @@ public class DBImporter {
 	 * @throws SQLException
 	 * @throws IOException
 	 */
-	private void loadSampleData(File dataDir, String dbName, String tableName,
+	private void loadSampleData(File dataDir, String tableName,
 			String extraColumnsWithTypes, String typeChar, String extraColumns,
 			Connection conn) throws SQLException, IOException {
 
@@ -300,8 +334,7 @@ public class DBImporter {
 				"CREATE TABLE %s(sample_id TEXT PRIMARY KEY,secondary_id TEXT,sampled_by TEXT,collection_date DATE,comments TEXT,category_id TEXT,iid TEXT,",
 				tmpTable) + extraColumnsWithTypes + ")");
 
-		copyFileToTable(dbName, tmpTable, new File(dataDir, tableName + ".csv"),
-				dataDir);
+		copyFileToTable2(conn, tmpTable, new File(dataDir, tableName + ".csv"));
 
 		st.executeUpdate(String.format(
 				"INSERT INTO samples (sample_id, secondary_id, sampled_by, collection_date, comments, category_id, iid, sample_type) "
@@ -474,71 +507,58 @@ public class DBImporter {
 	}
 
 	/**
-	 * Copy the data from a file named table.csv to the specified table in the
-	 * given database. The file must be in the specified directory.
+	 * Copy the data from a file named <b>table</b>.csv to the specified table
+	 * in the given database. The file must be in the specified directory.
 	 * 
-	 * @param dbName
-	 *            The database (Not null)
-	 * @param table
-	 *            The name of the table to import to (Not null)
+	 * @param conn
+	 *            The database connection (Not null)
 	 * @param dir
 	 *            The directory containing the table.csv file (Not null)
+	 * @param table
+	 *            The name of the table to import to (Not null)
 	 * @throws IOException
 	 *             Thrown if the file is not accessible
+	 * @throws SQLException
+	 *             Thrown if any SQL issues
 	 */
-	private void copyFileToTable(String dbName, String table, File dir)
-			throws IOException {
+	private void copyFileToTable(Connection conn, File dir, String table)
+			throws IOException, SQLException {
 
 		File csvFile = new File(dir, table + ".csv");
 
-		copyFileToTable(dbName, table, csvFile, dir);
+		copyFileToTable2(conn, table, csvFile);
 
 	}
 
 	/**
-	 * @param dbName
+	 * Append the data in the CSV file to the specified database table.
+	 * 
+	 * @param conn
+	 *            A database connection (Not null)
 	 * @param table
+	 *            A database table (Not null)
 	 * @param csvFile
+	 *            A CSV file with columns that match the database table (Not
+	 *            null)
 	 * @throws IOException
+	 *             Thrown if the file is not accessible
+	 * @throws SQLException
+	 *             Thrown if any SQL issues
 	 */
-	private void copyFileToTable(String dbName, String table, File csvFile,
-			File parentDir) throws IOException {
+	private void copyFileToTable2(Connection conn, String table, File csvFile)
+			throws IOException, SQLException {
 
-		String user = props.get(JDBC_USER);
-		String pass = props.get(JDBC_PASSWORD);
+		String pgCommand = String
+				.format("COPY %s FROM STDIN DELIMITER ',' CSV HEADER", table);
 
-		String pgCommand = String.format(
-				"\\copy %s FROM '%s' DELIMITER ',' CSV HEADER", table,
-				csvFile.getAbsolutePath());
+		CopyManager copyManager = new CopyManager((BaseConnection) conn);
 
-		String bashCommand = String.format(
-				"PGPASSWORD=%s psql -d %s -h localhost -U %s -c \"%s\"", pass,
-				dbName, user, pgCommand);
+		FileReader fileReader = new FileReader(csvFile);
 
-		System.out.println(bashCommand.replace(pass, "??????"));
+		System.out.println(pgCommand);
+		copyManager.copyIn(pgCommand, fileReader);
 
-		ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", bashCommand);
+		fileReader.close();
 
-		pb.redirectErrorStream(true);
-		Process p = pb.start();
-		BufferedReader input = new BufferedReader(
-				new InputStreamReader(p.getInputStream()));
-		String line;
-		while ((line = input.readLine()) != null) {
-			System.out.println(line);
-		}
-		input.close();
-
-		try {
-			p.waitFor(10, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			throw new IOException(
-					"Copy failed to finish in allotted timeout period.");
-		}
-
-		if (p.exitValue() != 0) {
-			throw new IOException(
-					"Copy exited with nonzero value.  It's likely the write did not succeed.");
-		}
 	}
 }
