f14ee69a483ba2753a8a0c6d64c9b7036d44c391
markroyer <mark.e.royer@gmail.com>
2017-02-04 15:58:07 -0500

    Simplify scale and crop

--- AntT/src/org/tephrochronology/Images.java
+++ AntT/src/org/tephrochronology/Images.java
@@ -3,9 +3,6 @@
  */
 package org.tephrochronology;
 
-import static java.lang.Math.min;
-import static java.lang.Math.round;
-
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
@@ -38,42 +35,33 @@ public class Images {
 		return out;
 	}
 	
-	public static BufferedImage scaleAndCrop(BufferedImage img, int newSize) {
+	public static BufferedImage scaleAndCrop(BufferedImage img, final int NEW_SIZE) {
 
 		float w = img.getWidth();
 		float h = img.getHeight();
 
 		float rat = w / h;
-
-		int MIN_SIZE = 75;
-
-		float newW = rat > 1 ? rat * MIN_SIZE : MIN_SIZE;
-		float newH = rat <= 1 ? rat * MIN_SIZE : MIN_SIZE;
-
-		BufferedImage newImg = scale(img, newW, newH);
-
-		float extra = rat > 1 ? newW - MIN_SIZE :  MIN_SIZE - newH;
-
-		int x = 0;
-		int y = 0;
-		int finalW = 0;
-		int finalH = 0;
 		
-		// Width greater than height crop sides
-		if (rat > 1) {
-			x = (int) extra / 2;
-			y = 0;
-			finalW = min(round(newW - extra / 2), MIN_SIZE);
-			finalH = min(round(newH), MIN_SIZE);
-		} else {
-			x = 0;
-			y = (int) extra / 2;
-			finalW = min(round(newW), MIN_SIZE);
-			finalH = min(round(newH - extra / 2), MIN_SIZE);
+		float newW = NEW_SIZE;
+		float newH = NEW_SIZE;
+		int x = 0;
+		int y = 0;		
+
+		if (rat > 1) { // w > h
+			newW = rat * NEW_SIZE;
+			// X needs to be translated by the difference
+			x = (int)(newW - NEW_SIZE)/2;
+		} else { // w <= h
+			newH = newW / rat;
+			// Y needs to be translated by the difference
+			y = (int)(newH - NEW_SIZE)/2;
 		}
 		
-		return newImg.getSubimage(x,y,finalW,finalH);
+		BufferedImage newImg = scale(img, newW, newH);
 
+		BufferedImage cropped = newImg.getSubimage(x,y,NEW_SIZE,NEW_SIZE);
+		
+		return cropped;
 	}
 	
 
