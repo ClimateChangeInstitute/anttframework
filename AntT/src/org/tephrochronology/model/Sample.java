36f0e74356da085f6e2e2acac65bcadc34c05327
markroyer <mark.e.royer@gmail.com>
2017-02-04 19:09:43 -0500

    Make sure samples with missing instruments are selected

--- AntT/src/org/tephrochronology/model/Sample.java
+++ AntT/src/org/tephrochronology/model/Sample.java
@@ -37,10 +37,11 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 @XmlRootElement
 //@formatter:off
 @NamedQuery(name = Sample.QUERY_GET_SAMPLE_INFO, query = 
-  "SELECT NEW org.tephrochronology.model.SampleInfo(TYPE(s), "
-+ "s.sampleID, s.secondaryID, s.sampledBy, s.comments, "
-+ "s.collectionDate, s.category.categoryID, s.instrument.id) "
-+ "FROM Sample s "
+"SELECT NEW org.tephrochronology.model.SampleInfo(TYPE(s), "
+		+ "s.sampleID, s.secondaryID, s.sampledBy, s.comments, "
+		+ "s.collectionDate, c.categoryID, iid.id) "
++ "FROM Sample s LEFT JOIN s.instrument iid "
++ "LEFT JOIN s.category c "
 + "ORDER BY TYPE(s), s.sampleID, s.collectionDate")
 //@formatter:on
 public abstract class Sample implements Serializable {
