ccb4ac34c956a0873800276d673f3bcc7796be05
markroyer <mark.e.royer@gmail.com>
2017-02-18 11:52:21 -0500

    Add format to search query results

--- AntT/WebContent/js/results.module.js
+++ AntT/WebContent/js/results.module.js
@@ -1,94 +1,94 @@
-var app = angular.module('exampleApp', [ 'cgBusy' ]);
+var app = angular.module('exampleApp', ['cgBusy']);
 
-app.factory('dataSource', [ '$http', function($http) {
-	var factory = [];
+app.factory('dataSource', ['$http', function($http) {
+    var factory = [];
 
-	factory.getData = function() {
-		return $http.get("generated/allSamples.xml");
-	};
-	
-	factory.getChemistryOrder = function() {
-		return $http.get("chemistries_order.txt");
-	};
+    factory.getData = function() {
+        return $http.get("generated/allSamples.xml");
+    };
 
-	return factory;
-} ]);
+    factory.getChemistryOrder = function() {
+        return $http.get("chemistries_order.txt");
+    };
+
+    return factory;
+}]);
 
 app.filter('unsafe', function($sce) {
-	return function(val) {
-		return $sce.trustAsHtml(val);
-	};
+    return function(val) {
+        return $sce.trustAsHtml(val);
+    };
 });
 
 app.filter('encode', function() {
-	return window.encodeURIComponent;
+    return window.encodeURIComponent;
 });
 
 /**
  * A filter to remove special characters in ID strings.
  */
 function makeID(str) {
-	return str.replace(/\s|-|\./g, "_");
+    return str.replace(/\s|-|\./g, "_");
 }
 
 app.filter('makeID', function() {
-	return makeID;
+    return makeID;
 });
 
 app.directive('tephraDownload', function() {
-	return {
-		restrict : 'A',
-		scope : true,
-		link : function(scope, element, attrs) {
-
-			function download(evt) {
-				// Find out which sample IDs are selected for download
-				var selectedIds = [];
-				var simCoefficients = [];
-				$.each($(this).parents(".panel").find("input.sample-select-box:checked"), function(
-						i, e) {
-					var id = $(e).attr("id").split('-')[2];
-					selectedIds.push(id);
-					simCoefficients.push($(e).parents(".panel-samples").find(".simCoefficient")[0].innerHTML);
-				});
-
-				if (selectedIds.length <= 0) {
-					alert("No sample results selected.");
-					return;
-				}
-
-				// Go get the matching MMElements
-				var selectedMMElements = [];
-				$.each(app.allMMElements, function(i, e) {
-					if (selectedIds.indexOf(e.sampleID) >= 0)
-						selectedMMElements.push(e);
-				});
-
-				antt.loadChemistriesOrder("chemistries_order.txt", function(
-						order) {
-
-					var now = new Date();
-
-					var dateString = now.getFullYear() + '-'
-							+ (now.getMonth() + 1) + '-' + now.getDate() + '_'
-							+ now.getHours() + '-' + now.getMinutes();
-
-					var downloadFileName = "anttSearch" + dateString + ".csv";
-					antt.saveSelectedData(downloadFileName, selectedMMElements, simCoefficients, order);
-				});
-
-			}
-
-			element.on('click', download);
-		}
-	};
-});
-
-app.directive('tooltip', function(){
     return {
         restrict: 'A',
-        link: function(scope, element, attrs){
-        	$(element).tooltip();
+        scope: true,
+        link: function(scope, element, attrs) {
+
+            function download(evt) {
+                // Find out which sample IDs are selected for download
+                var selectedIds = [];
+                var simCoefficients = [];
+                $.each($(this).parents(".panel").find("input.sample-select-box:checked"), function(
+                    i, e) {
+                    var id = $(e).attr("id").split('-')[2];
+                    selectedIds.push(id);
+                    simCoefficients.push($(e).parents(".panel-samples").find(".simCoefficient")[0].innerHTML);
+                });
+
+                if (selectedIds.length <= 0) {
+                    alert("No sample results selected.");
+                    return;
+                }
+
+                // Go get the matching MMElements
+                var selectedMMElements = [];
+                $.each(app.allMMElements, function(i, e) {
+                    if (selectedIds.indexOf(e.sampleID) >= 0)
+                        selectedMMElements.push(e);
+                });
+
+                antt.loadChemistriesOrder("chemistries_order.txt", function(
+                    order) {
+
+                    var now = new Date();
+
+                    var dateString = now.getFullYear() + '-' +
+                        (now.getMonth() + 1) + '-' + now.getDate() + '_' +
+                        now.getHours() + '-' + now.getMinutes();
+
+                    var downloadFileName = "anttSearch" + dateString + ".csv";
+                    antt.saveSelectedData(downloadFileName, selectedMMElements, simCoefficients, order);
+                });
+
+            }
+
+            element.on('click', download);
+        }
+    };
+});
+
+app.directive('tooltip', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            $(element).tooltip();
         }
     };
 });
@@ -96,96 +96,107 @@ app.directive('tooltip', function(){
 
 app.controller('results', function($scope, dataSource) {
 
-	$scope.AppController = [];
+    $scope.AppController = [];
 
-	// Anything after this value will be below 'Orig. Total'
-	var divider = "H2O-";
-	
-	// If we decide not to use default behavior, let's use this alternative
-	// behavior
-	// $scope.delay = 0;
-	// $scope.minDuration = 500;
-	$scope.message = 'Loading samples. Please wait...';
-	// $scope.templateUrl = 'angular-busy.html';
-	// $scope.backdrop = true;
-	// $scope.promise = null;
+    // Anything after this value will be below 'Orig. Total'
+    var divider = "H2O-";
 
-	$scope.promise = antt.loadMMElements('generated/allMMElements.xml', function(
-			allMMElements) {
-		app.allMMElements = allMMElements;
+    // If we decide not to use default behavior, let's use this alternative
+    // behavior
+    // $scope.delay = 0;
+    // $scope.minDuration = 500;
+    $scope.message = 'Loading samples. Please wait...';
+    // $scope.templateUrl = 'angular-busy.html';
+    // $scope.backdrop = true;
+    // $scope.promise = null;
 
-		var values = antt.getUrlParameters();
-		var searches = antt.getQueriedElements(values, 's');
+    $scope.promise = antt.loadMMElements('generated/allMMElements.xml', function(
+        allMMElements) {
+        app.allMMElements = allMMElements;
 
-		var allResults = [];
+        var values = antt.getUrlParameters();
+        var searches = antt.getQueriedElements(values, 's');
 
-		$.each(searches, function(i, s) {
+        var allResults = [];
 
-			var filtered = antt.filterMMElements(Object.keys(s), allMMElements, '%');
+        var symbolFormat = {};
 
-			var searchRes = [];
+        dataSource.getChemistryOrder().success(function(data) {
+            var elementOrder = data.split('\n').filter(function(str) {
+                return !str.startsWith("#") && str.length > 0;
+            });
 
-			$.each(filtered, function(i, e) {
+            $.each(searches, function(i, s) {
 
-				var simVal = antt.statistics.similarityCoefficientListList($.map(s, function(
-						val, key) {
-					return val;
-				}), antt.valuesArray(Object.keys(s), e));
+                var filtered = antt.filterMMElements(Object.keys(s), allMMElements, '%');
 
-				e.primaryElementData = [];
-				e.secondaryElementData = [];
+                var searchRes = [];
 
-				dataSource.getChemistryOrder().success(function(data) {
-					var elementOrder = data.split('\n').filter(function(str){
-						return !str.startsWith("#") && str.length > 0;
-					});
-					
-					var dividerIndex = elementOrder.indexOf(divider);
-					$.each(e.elementData, function(i, val) {
-						var i = elementOrder.indexOf(val.symbol);
-						if (0 <= i && i <= dividerIndex) {
-							val.order = i;
-							e.primaryElementData.push(val);
-						} else {
-							e.secondaryElementData.push(val);
-						}
-					});
+                $.each(filtered, function(i, e) {
 
-				});
-				
-				searchRes.push({
-					simVal : simVal,
-					mme : e
-				});
-				
-				
-			});
-			
-			searchRes.sort(function(a, b) {
-				return b.simVal - a.simVal;
-			});
+                    var simVal = antt.statistics.similarityCoefficientListList($.map(s, function(
+                        val, key) {
+                        return val;
+                    }), antt.valuesArray(Object.keys(s), e));
 
-			searchQuery = "";
+                    e.primaryElementData = [];
+                    e.secondaryElementData = [];
 
-			$.each(s, function(a, b) {
-				searchQuery += a + " = " + b + ",";
-			});
+                    var dividerIndex = elementOrder.indexOf(divider);
+                    $.each(e.elementData, function(i, val) {
+                        var i = elementOrder.indexOf(val.symbol);
+                        if (0 <= i && i <= dividerIndex) {
+                            val.order = i;
+                            e.primaryElementData.push(val);
+                        } else {
+                            e.secondaryElementData.push(val);
+                        }
+                        if (!symbolFormat[val.symbol])
+                            symbolFormat[val.symbol] = val.format;
+                    });
 
-			searchQuery = searchQuery.slice(0, -1);
+                    searchRes.push({
+                        simVal: simVal,
+                        mme: e
+                    });
 
-			allResults.push({
-				searchRes : searchRes,
-				searchQuery : searchQuery,
-				count : i
-			});
 
-		});
+                });
 
-		// reverse array so most recent search is displayed first
-		allResults.reverse();
+                searchRes.sort(function(a, b) {
+                    return b.simVal - a.simVal;
+                });
 
-		$scope.filtered = allResults;
-		$scope.$apply();
+                /**
+                 * @param {object} Search query object (Not null)
+                 * @return {string} A search query string of the form k1 = v1, ... , kn = vn
+                 */
+                function createSearchQueryString(searchObj) {
+                    searchQuery = "";
 
-	});
+                    // Process each key value pair
+                    $.each(searchObj, function(a, b) {
+                        searchQuery += symbolFormat[a] + " = " + b + ",";
+                    });
+                    searchQuery = searchQuery.slice(0, -1);
+
+                    return searchQuery;
+                };
+                allResults.push({
+                    searchRes: searchRes,
+                    searchQuery: createSearchQueryString(s),
+                    count: i
+                });
+
+            });
+
+            // reverse array so most recent search is displayed first
+            allResults.reverse();
+
+            $scope.filtered = allResults;
+        });
+
+        $scope.$apply();
+
+    });
 });
